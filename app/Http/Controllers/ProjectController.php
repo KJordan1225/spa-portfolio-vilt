e498979bc88301f2bde1e53f99829b2d17c7d712
Keith Jordan <shadow902@gmail.com>
2022-06-28 13:52:33 -0400

    added display Projects to Admin panel - icon colors not working yet

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Project;
+use Inertia\Inertia;
 
 class ProjectController extends Controller
 {
-    //
+    public function index() {
+        return Inertia::render('Projects/All', [
+            'projects' => Project::all(),
+        ]);
+    }
 }
